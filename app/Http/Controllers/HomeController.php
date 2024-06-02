f026df7b573e8171807278ab919b98d905a636af
Darkcloud1224 <127832687+Darkcloud1224@users.noreply.github.com>
2024-06-02 12:11:50 +0800

    route fixed
    landing page fix

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,14 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        if (Auth::check()) {
+                    /** @var \App\Models\User */
+            $user = Auth::user();
+            if ($user->hasRole('supervisor')) {
+                return redirect('/supervisor');
+            } elseif ($user->hasRole('volunteer')) {
+                return redirect('/volunteer/dashboard');
+            }
+        }
     }
 }
